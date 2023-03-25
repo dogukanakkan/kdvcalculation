9ed984b21cc368cde1f294104bbd718bcc37c7d5
dogukanakkan <dogukanakkan3@gmail.com>
2023-03-25 15:42:00 +0300

    www.patika.dev

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        double tutar, kdvOran = 0.18, kdvTutar, kdvliTutar;
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("Ücret Tutarını Giriniz : ");
-        tutar = input.nextDouble();
-
-        kdvOran = (tutar>0) && (tutar<=1000) ? 0.18 : 0.08;
-
-        kdvTutar = tutar * kdvOran;
-        kdvliTutar = tutar + kdvTutar;
-
-        System.out.println("KDV'siz Tutar : " + tutar);
-        System.out.println("KDV Oranı : " + kdvOran);
-        System.out.println("KDV Tutarı : " + kdvTutar);
-        System.out.println("KDV'li Tutarı : " + kdvliTutar);
+        int a, b;
+        double c;
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("1. Kenarı Giriniz : ");
+        a = girdi.nextInt();
+        System.out.print("2. Kenarı Giriniz : ");
+        b = girdi.nextInt();
+        c = Math.sqrt((a*a) + (b*b));
+        System.out.println("Hipotenüs : " + c);
 
 
-        System.out.println(kdvliTutar);
 
     }
 }
